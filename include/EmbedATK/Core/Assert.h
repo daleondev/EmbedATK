ac430d2d6ef94a47005b9f575948a6a69cc09684
daleondev <david@rodenheber.de>
2025-11-16 15:43:11 +0100

    Assert

--- include/EmbedATK/Core/Assert.h
+++ include/EmbedATK/Core/Assert.h
@@ -6,9 +6,21 @@
     #if defined(EATK_PLATFORM_WINDOWS)
         #include <intrin.h>
         #define EATK_DEBUG_BREAK() __debugbreak()
-    #elif defined(EATK_PLATFORM_LINUX) || defined(EATK_PLATFORM_ARM)
+    #elif defined(EATK_PLATFORM_LINUX)
         #include <csignal>
         #define EATK_DEBUG_BREAK() std::raise(SIGTRAP)
+    #elif defined(EATK_PLATFORM_ARM)
+        #if defined(__GNUC__) && !defined(__llvm__) // GCC
+            #define EATK_DEBUG_BREAK() __asm volatile("bkpt #0")
+        #elif defined(__llvm__) // Clang
+            #define EATK_DEBUG_BREAK() __builtin_trap()
+        #elif defined(__CC_ARM) // Keil MDK
+            #define EATK_DEBUG_BREAK() __bkpt(0)
+        #elif defined(__ICCARM__) // IAR
+            #define EATK_DEBUG_BREAK() __DebugBreak()
+        #else
+            #define EATK_DEBUG_BREAK() std::abort()
+        #endif
     #else
         #define EATK_DEBUG_BREAK() std::abort()
     #endif
